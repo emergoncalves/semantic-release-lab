42a68a3b54dd04a6136da299fad8cedbe50d60ef
Emerson Gonçalves <emerson.u.g@gmail.com>
2023-04-20 14:11:30 -0300

    fix: Adjust text(2)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello World </h1>
+    <h1>Hello World</h1>
     <p>
       <Link href="/about">About us </Link>
     </p>
